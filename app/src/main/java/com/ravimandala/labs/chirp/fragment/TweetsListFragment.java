d73ccee93c0bc73942787d07d9940ca5bb6e9e93
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2016-02-27 16:50:15 -0800

    Adding fragment, not linked yet

---
+++ app/src/main/java/com/ravimandala/labs/chirp/fragment/TweetsListFragment.java
@@ -0,0 +1,9 @@
+package com.ravimandala.labs.chirp.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+public class TweetsListFragment extends Fragment {
+    public TweetsListFragment() {
+    }
+}
